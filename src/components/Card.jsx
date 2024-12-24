c32d223d87b2432e499105fa357935d198822670
WASBIS <abidardiyanto10@gmail.comS>
2024-12-24 20:55:47 +0700

    add font into library

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,15 +4,12 @@ import React from "react";
 const Card = ({ title, description, imageUrl, link, footer, className, children }) => {
     return (
         <div
-            className={`relative group rounded-lg overflow-hidden shadow-lg bg bg-white dark:bg-gray-800  backdrop-blur-sm transition-all ease-in-out duration-300 hover:scale-105 hover:cursor-pointer ${className}`}
+            className={`relative group rounded-2xl overflow-hidden shadow-lg bg bg-white dark:bg-gray-800  backdrop-blur-sm transition-all ease-in-out duration-300 hover:scale-105 hover:cursor-pointer ${className}`}
         >
-            {/* Border gradien dengan animasi */}
-            <div className="absolute inset-0 rounded-lg p-[2px] group-hover:bg-gradient-to-r from-[#CF7401] via-[#A73C84] to-[#CF7401] bg-[length:200%_200%]  opacity-100 ">
-                {/* Lapisan isi untuk menutupi bagian dalam border */}
+            <div className="absolute inset-0 rounded-2xl p-[2px] group-hover:bg-gradient-to-r from-[#CF7401] via-[#A73C84] to-[#CF7401] bg-[length:200%_200%]  opacity-100 ">
                 <div className="h-full w-full scale-98 bg-white blur-sm  dark:bg-gray-800 rounded-lg"></div>
             </div>
 
-            {/* Konten kartu */}
             <div className="relative z-10 p-4">
                 {imageUrl && <img className="w-full h-48 object-cover rounded-t-lg" src={imageUrl} alt={title} />}
                 {title && <h2 className="text-xl font-semibold text-gray-800 dark:text-white">{title}</h2>}
